7602c71ece8c092d280c88664b1383a0fbdcb75c
Youssef <140279042+aliveAndYoung@users.noreply.github.com>
2026-05-09 01:26:32 +0300

    added inference logic

---
+++ src/try_out.py
@@ -0,0 +1,56 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+from PIL import Image
+
+
+def apply_pid_algorithm(image):
+    """
+    Extracts Pixelwise Decomposition Residuals by mapping to YUV space,
+    quantizing, and returning the difference from the original.
+    """
+    img = np.array(image)
+    if img.ndim == 2:
+        img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+    elif img.shape[2] == 4:
+        img = cv2.cvtColor(img, cv2.COLOR_RGBA2RGB)
+    img_float = img.astype(np.float32)
+    Mt = np.array([
+        [0.299, 0.587, 0.114],
+        [-0.168736, -0.331264, 0.5],
+        [0.5, -0.418688, -0.081312]
+    ])
+    pixels = img_float.reshape(-1, 3)
+    yuv_pixels = np.dot(pixels, Mt.T) 
+    yuv_quantized = np.floor(yuv_pixels)
+    Mt_inv = np.array([
+        [1.0, 0.0, 1.402],
+        [1.0, -0.344136, -0.714136],
+        [1.0, 1.772, 0.0]
+    ])
+    recovered_pixels = np.dot(yuv_quantized, Mt_inv.T)
+    clipped_pixels = np.clip(recovered_pixels, 0, 255).astype(np.float32)
+    img_altered = clipped_pixels.reshape(img.shape)
+    residual = img_float - img_altered
+    residual_resized = cv2.resize(residual, (224, 224), interpolation=cv2.INTER_CUBIC)
+    return residual_resized
+
+
+
+if __name__ == "__main__":
+    # Example usage
+    image_path = "F:/projects/PiD_AIGID/images/real_1.jpg"
+
+        # 2. Load it using cv2 (Result: BGR array)
+    image = Image.open(image_path)
+    fig = plt.figure("PID Algorithm Analysis")
+
+    plt.subplot(1, 2, 1)
+    plt.title("Before")
+    plt.imshow(image)
+    res= apply_pid_algorithm(image)
+    plt.subplot(1, 2, 2)
+    plt.title("After")
+    plt.imshow(res)
+
+    plt.show()
